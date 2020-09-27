ac2d8251e4c3d21c6582c3230ff33b739eebe499
denpong_de <denpong_de@users.noreply.github.com>
2020-09-27 17:37:10 +0700

    Add run animation, rigidbody, make tilemap static
    Also script with GetComponent. ready for prototype. For real this time!

---
+++ 2DGameMidterm/Assets/!Scripts/player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody2D Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
