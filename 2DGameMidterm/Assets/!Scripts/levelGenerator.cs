a23ab97986f7de97e6256f2ced4828d05e8c5af4
denpong_de <denpong_de@users.noreply.github.com>
2020-09-28 21:56:43 +0700

    generate sprite up

--- 2DGameMidterm/Assets/!Scripts/levelGenerator.cs
+++ 2DGameMidterm/Assets/!Scripts/levelGenerator.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 
 public class levelGenerator : MonoBehaviour
 {
+    [SerializeField] private Transform levelPart_Start;
     [SerializeField] private Transform levelPart_1;
 
     private void Awake()
     {
-        spawnLevelPart(new Vector3(9, -3));
-        spawnLevelPart(new Vector3(9, -3) + new Vector3(18,0));
-        spawnLevelPart(new Vector3(9, -3) + new Vector3(18 + 18, 0));
+        Transform lastLevelPartTransform;
+        lastLevelPartTransform = spawnLevelPart(levelPart_Start.Find("EndPosition").position);
+        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
     }
 
-    private void spawnLevelPart(Vector3 spawnPosition)
+    private Transform spawnLevelPart(Vector3 spawnPosition)
     {
-        Instantiate(levelPart_1, spawnPosition, quaternion.identity);
+       Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, quaternion.identity);
+        return levelPartTransform;
     }
 }
