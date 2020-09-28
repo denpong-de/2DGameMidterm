6fbdcdebe36ba25f662685f384b87cccdbfa7a20
denpong_de <denpong_de@users.noreply.github.com>
2020-09-29 00:42:34 +0700

    infinite run finished but have only one prefabs
    +can gen infinite platform
    -cannot delete old platform
    -cannot random levelpart

--- 2DGameMidterm/Assets/!Scripts/levelGenerator.cs
+++ 2DGameMidterm/Assets/!Scripts/levelGenerator.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 
 public class levelGenerator : MonoBehaviour
 {
+    private const float LevelSpawnDistance = 25f;
+
     [SerializeField] private Transform levelPart_Start;
     [SerializeField] private Transform levelPart_1;
+    [SerializeField] private player player;
+
+    private Vector3 lastEndPosition;
 
     private void Awake()
     {
-        Transform lastLevelPartTransform;
-        lastLevelPartTransform = spawnLevelPart(levelPart_Start.Find("EndPosition").position);
-        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
-        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
-        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
-        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
-        lastLevelPartTransform = spawnLevelPart(lastLevelPartTransform.Find("EndPosition").position);
+        lastEndPosition = levelPart_Start.Find("EndPosition").position;
     }
 
-    private Transform spawnLevelPart(Vector3 spawnPosition)
+    private void Update()
+    {
+        if(Vector3.Distance(player.transform.position,lastEndPosition) < LevelSpawnDistance)
+        {
+            SpawnLevelPart();
+        }
+    }
+
+    private void SpawnLevelPart()
+    {
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
     {
        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, quaternion.identity);
         return levelPartTransform;
