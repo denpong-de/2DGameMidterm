6674ee6815c6ea7c2c1e491b711fc249926007c8
denpong_de <denpong_de@users.noreply.github.com>
2020-09-28 17:52:27 +0700

    Add Jump Animation

--- 2DGameMidterm/Assets/!Scripts/player.cs
+++ 2DGameMidterm/Assets/!Scripts/player.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
-{
-    Rigidbody2D Rigidbody;
+{ 
     public playerValue Player; //ScriptableObject
 
-    // Start is called before the first frame update
+    Rigidbody2D Rigidbody;
+    Animator Animator;
+
     void Awake()
     {
         Rigidbody = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -24,15 +26,16 @@ public class player : MonoBehaviour
     public void pressJump(InputAction.CallbackContext context)
     {
         if(context.performed == true && canJump == true)
-        {
+        {      
             Rigidbody.AddForce(Vector2.up * Player.jumpVelocity, ForceMode2D.Impulse);
             canJump = false;
+            Animator.SetBool("isJump", true);
         }    
     }
 
     void betterGravityForJump()
     {
-        if(Rigidbody.velocity.y < 0)
+        if (Rigidbody.velocity.y < 0)
         {
             Rigidbody.gravityScale = Player.fallMultiply;
         }
@@ -46,6 +49,6 @@ public class player : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
-
+        Animator.SetBool("isJump", false);
     }
 }
