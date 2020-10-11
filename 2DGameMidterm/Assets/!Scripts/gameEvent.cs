8fb53e168079462710841b7206250ff98bfbdfaa
denpong_de <denpong_de@users.noreply.github.com>
2020-10-11 17:22:39 +0700

    create gameController
    where all Event system are operate.

---
+++ 2DGameMidterm/Assets/!Scripts/gameEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameEvent : MonoBehaviour
+{
+    public static gameEvent current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public Action onCoinTriggerEnter;
+    public void CoinTriggerEnter()
+    {
+        if(onCoinTriggerEnter != null)
+        {
+            onCoinTriggerEnter();
+        }
+    }
+}
