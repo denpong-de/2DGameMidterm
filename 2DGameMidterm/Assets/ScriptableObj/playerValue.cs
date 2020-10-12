4d1a127ab4e1469cb7683d9faebe82d991a403d3
denpong_de <denpong_de@users.noreply.github.com>
2020-10-13 01:42:56 +0700

    +Difficult is now control by playerValue

--- 2DGameMidterm/Assets/ScriptableObj/playerValue.cs
+++ 2DGameMidterm/Assets/ScriptableObj/playerValue.cs
@@ -5,27 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Player", menuName = "player")]
 public class playerValue : ScriptableObject
 {
-    //1.
     [Header("Player Settings")]
     [Range(0f, 10f)] public float jumpVelocity;
     [Range(0f, 5f)] public float fallMultiply;
     [Range(0f, 20f)] public float runSpeed;
 
-    //2.
     [Header("Level Generator")]
     [Range(0f, 50f)] public float SpawnDistance;
 
-    //3.
     [Header("BG Setting")]
     public Vector2 bgPropMultiplier;
     public Vector2 bgMultiplier;
 
-    //4.
-    [Header("Game price")]
+    [Header("Difficult Value")]
+    public int easyToNormal;
+
+    [Header("In Game price")]
     public int extraLifePrice;
 
-    //5.
-    [Header("InGame Value")]
+    [Header("In Game Value")]
     public int coinCount;
     public Vector3 BeforeDeadPosition;
 }
